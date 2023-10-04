697b057cb09ed90fe4739bec826b9a05287a5ea4
Ravendra1236 <97697163+Ravendra1236@users.noreply.github.com>
2023-10-04 01:25:51 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,14 @@
+//  Basic we already know
+
+ console.log(null>0);       //false
+ console.log(nul == 0);  //false
+ console.log(num >=0);    //true
+
+ console.log(undefined == 0);
+ console.log(undefined>0);
+ console.log(undefined <0);
+
+
+ //  === strict check
+
+ console.log("2" ===2);
