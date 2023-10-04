a94681816d7d731f99d6e9acd889281ae8f7a649
Ravendra1236 <97697163+Ravendra1236@users.noreply.github.com>
2023-10-04 01:29:45 +0000

    04_comparisions

--- 01_basics/04_comparisions.js
+++ 01_basics/04_comparisions.js
@@ -1,14 +1,14 @@
 //  Basic we already know
 
  console.log(null>0);       //false
- console.log(nul == 0);  //false
- console.log(num >=0);    //true
+ console.log(null == 0);  //false
+ console.log(null >=0);    //true
 
  console.log(undefined == 0);
  console.log(undefined>0);
  console.log(undefined <0);
 
-
+// Avoid these types of comparisions 
  //  === strict check
 
  console.log("2" ===2);
