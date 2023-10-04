5901ebd72600e5fdd15be45aa9514712d1324b9c
Ravendra1236 <97697163+Ravendra1236@users.noreply.github.com>
2023-10-04 00:45:52 +0000

    03_coversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33" ;
+
+console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" ==> NaN Not a Number
+//  true ==> 1
+//  false ==> 0
+
+let isLoggedIn = "Hello";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0=> false
+// "" ==> For empty it will return false
+// "Ravendra" => true
+
+let stringNumber = "33";
+console.log(stringNumber);
+console.log(typeof stringNumber);
