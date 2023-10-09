c67f948132d0dcae56e20a25858543aa441ffa5a
Ravendra1236 <97697163+Ravendra1236@users.noreply.github.com>
2023-10-09 00:43:58 +0000

    02_scopes

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -7,9 +7,54 @@ if(true){
     let a = 10;         // Local Scope 
     const b = 20 ;
     // var c = 40 ;
-    console.log("Inner: " , a);
+    // console.log("Inner: " , a);
 }
 
-console.log(a);         
+// console.log(a);         
 // console.log(b);
 // console.log(c);
+
+
+// Child Function access variable of parent functon
+
+function one (){
+    const username = "Ravendra"
+
+    function two () {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+// one();
+
+if(true){
+    const username = "Ravendra"
+    if(username ==="Ravendra"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+
+    // console.log(website);        // error
+}
+// console.log(username);    // Error
+
+
+// ++++++++++++++++++++ Intersting ++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1 ;
+}
+console.log(addone(5))
+
+
+// console.log(addTwo(6))          // error becuase we have hold function into a variable
+const addTwo = function(num){       
+    return + 2
+}
+
+// Further we will study in hoisting
